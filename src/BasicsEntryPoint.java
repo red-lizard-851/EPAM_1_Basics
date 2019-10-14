19855bc3d02a7a21ca9d929893ab1d13449dfb65
red-lizard-851 <47646130+red-lizard-851@users.noreply.github.com>
2019-10-14 20:24:04 +0300

    First commit of working prototype

---
+++ src/BasicsEntryPoint.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+
+public class BasicsEntryPoint {
+
+    public static void main (String[] args) {
+
+        BasicsLinear1.calculate();
+        BasicsLinear2.calculate();
+
+    }
+}
